cf6546a92b8fbb5df8787c1b7a2f518bb42ef859
ngf93 <nabiullinaguzel93@gmail.com>
2022-09-21 19:40:33 +0300

    Feature: account history

--- src/pages/account/History.js
+++ src/pages/account/History.js
@@ -4,6 +4,21 @@ export default function History(props) {
     return (
         <div className='box p-5'>
             <h1>История заказов</h1>
+            <div className='order'>
+                <div className='title'>
+                    <div>
+                        <span>№ 113</span>
+                        <span>от 1.11.2020</span>
+                    </div>
+                    <div>1 250 ₽</div>
+                </div>
+                <div className='main'>
+                    <div>
+                        <a href='/'>Термостакан «Любимая подруга. Цветы», 350 мл</a>
+                        <span>500 ₽</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
